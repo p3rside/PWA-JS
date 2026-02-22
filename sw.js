8d83af9a1f5f0e37f2b24107b552479c4a23ad29
p3rside <134974325+p3rside@users.noreply.github.com>
2026-02-22 20:19:24 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,57 +1,31 @@
-const cacheName = 'piac-pwa-v1';
+const cacheName = 'piac-pwa-v2'; // Zmień wersję przy aktualizacji
 const filesToCache = [
- '/',
- '/index.html',
- '/style.css',
- '/js/main.js',
- '/images/favicon-96x96.png'
+ './',
+ './index.html',
+ './style.css',
+ './js/main.js',
+ './site.webmanifest',
+ './images/web-app-manifest-192x192.png'
 ];
 
-// Instalacja i buforowanie plików statycznych
 self.addEventListener('install', (event) => {
  event.waitUntil(
-  caches.open(cacheName).then((cache) => {
-   return cache.addAll(filesToCache);
-  })
+  caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
  );
 });
 
-// Obsługa zapytań i buforowanie dynamiczne
 self.addEventListener('fetch', (event) => {
  event.respondWith(
   caches.match(event.request).then((response) => {
-   // Zwróć z cache lub pobierz z sieci
    return response || fetch(event.request).then((fetchResponse) => {
-    // Buforuj tylko udane zapytania GET
-    if (event.request.method === 'GET') {
-     return caches.open(cacheName).then((cache) => {
+    return caches.open(cacheName).then((cache) => {
+     // Dynamiczne cache'owanie plików GET
+     if (event.request.method === 'GET') {
       cache.put(event.request, fetchResponse.clone());
-      return fetchResponse;
-     });
-    }
-    return fetchResponse;
-   });
-  }).catch(() => {
-   // Fallback dla braku połączenia przy nawigacji
-   if (event.request.mode === 'navigate') {
-    return caches.match('/index.html');
-   }
-  })
- );
-});
-
-// Aktywacja i czyszczenie starego cache
-self.addEventListener('activate', (event) => {
- const cacheWhitelist = [cacheName];
- event.waitUntil(
-  caches.keys().then((cacheNames) => {
-   return Promise.all(
-    cacheNames.map((cache) => {
-     if (!cacheWhitelist.includes(cache)) {
-      return caches.delete(cache);
      }
-    })
-   );
-  })
+     return fetchResponse;
+    });
+   });
+  }).catch(() => caches.match('./index.html')) // Fallback offline
  );
 });
